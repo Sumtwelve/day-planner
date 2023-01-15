bf236c9fab45fd02e32bb0b4445b648e7d295e2e
sumtwelve <sumtwelve@gmail.com>
2023-01-15 14:28:15 -0700

    finished all acceptance criteria

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
+// TODO: IDEAS!!!
+// A "save all" button
+// an auto-save feature? When you type it just waits a second or two then saves it
+// A delete-all button that clears local storage
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   
+  // PUT CURRENT DAY IN THE H1
+  var currentDayEl = $("#currentDay");
+  var now = dayjs().format("MMMM DD, YYYY");
+  currentDayEl.text("Today's Date: " + now);
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -36,6 +48,16 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  for (var i = 0; i < rows.length; i++) {
+    var rowHour = i + 7; // I need i to be 0, but current row's hour will always be i + 7
+    var itemTitle = ("hour-" + rowHour);
+    var rowData = (localStorage.getItem(itemTitle));
+    console.log(itemTitle + ": " + rowData + " (" + typeof rowData + ")");
+    if (rowData != null) {
+      rows.eq(i).children().eq(1).val(rowData);
+    }
+  }
+
 
 
 
@@ -47,7 +69,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $(".saveBtn").on("click", function(event) {
+  $(".saveBtn").on("click", function() {
     // get the hour
     var parentDiv = $(this).parent();
     var hour = parseInt(parentDiv.attr("id").split("-")[1]);
@@ -59,6 +81,21 @@ $(function () {
     localStorage.setItem(hourItem, textAreaContent);
   });
 
+  $("textarea").keypress(function(event) {
+    if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
+      // get the hour
+      var parentDiv = $(this).parent();
+      var hour = parseInt(parentDiv.attr("id").split("-")[1]);
+      console.log("User did CTRL+ENTER for hour " + hour);
+
+      // now save the textarea content to local storage
+      var textAreaContent = parentDiv.children().eq(1).val();
+      var hourItem = ("hour-" + hour);
+      localStorage.setItem(hourItem, textAreaContent);
+    }
+  })
+
+
 
 
   
@@ -66,4 +103,6 @@ $(function () {
 
   //
   // TODO: Add code to display the current date in the header of the page.
+  // 
+
 });
